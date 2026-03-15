e7aab1d7d774c8f84da17cd912394485d3a1b4d2
Justin Tobler <jltobler@gmail.com>
2026-03-15 18:17:23 -0500

    series: introduce "log" subcommand

--- builtin/series.c
+++ builtin/series.c
@@ -1,13 +1,17 @@
 #include "builtin.h"
 #include "hash.h"
+#include "hex.h"
+#include "log-tree.h"
 #include "object-name.h"
 #include "parse-options.h"
 #include "refs.h"
+#include "revision.h"
 #include "strbuf.h"
 
 static const char *const series_usage[] = {
 	"git series create <series-name>",
 	"git series list",
+	"git series log",
 	NULL
 };
 
@@ -109,6 +113,75 @@ static int cmd_series_list(int argc, const char **argv, const char *prefix,
 	return 0;
 }
 
+static char *get_current_series(struct repository *repo)
+{
+	struct strbuf referent = STRBUF_INIT;
+	struct strbuf base = STRBUF_INIT;
+	const char *series;
+	char *result = NULL;
+
+	if (refs_read_symbolic_ref(get_main_ref_store(repo), "HEAD", &referent) == NOT_A_SYMREF)
+		goto out;
+
+	if (!skip_prefix(referent.buf, "refs/heads/", &series))
+		goto out;
+
+	strbuf_addf(&base, "refs/series/%s/base", series);
+	if (!refs_ref_exists(get_main_ref_store(repo), base.buf))
+		goto out;
+
+	result = xstrdup(series);
+
+out:
+	strbuf_release(&referent);
+	strbuf_release(&base);
+
+	return result;
+}
+
+static int cmd_series_log(int argc, const char **argv, const char *prefix,
+			  struct repository *repo)
+{
+	struct strbuf base_ref = STRBUF_INIT;
+	struct object_id base;
+	struct commit *commit;
+	struct rev_info rev;
+	char *series;
+	struct option options[] = {
+		OPT_END()
+	};
+	argc = parse_options(argc, argv, prefix, options, series_usage, 0);
+	if (argc)
+		usage(_("too many arguments"));
+
+	repo_init_revisions(repo, &rev, prefix);
+	rev.commit_format = CMIT_FMT_MEDIUM;
+	rev.always_show_header = 1;
+	rev.verbose_header = 1;
+
+	series = get_current_series(repo);
+	if (!series)
+		die(_("not series found"));
+
+	strbuf_addf(&base_ref, "refs/series/%s/base", series);
+	repo_get_oid(repo, base_ref.buf, &base);
+
+	add_head_to_pending(&rev);
+	add_pending_oid(&rev, base_ref.buf, &base, UNINTERESTING);
+
+	if (prepare_revision_walk(&rev))
+		die(_("revision walk setup failed"));
+
+	while ((commit = get_revision(&rev)) != NULL)
+		log_tree_commit(&rev, commit);
+
+	release_revisions(&rev);
+	strbuf_release(&base_ref);
+	free(series);
+
+	return 0;
+}
+
 int cmd_series(int argc, const char **argv, const char *prefix,
 	       struct repository *repo)
 {
@@ -116,6 +189,7 @@ int cmd_series(int argc, const char **argv, const char *prefix,
 	struct option options[] = {
 		OPT_SUBCOMMAND("create", &fn, cmd_series_create),
 		OPT_SUBCOMMAND("list", &fn, cmd_series_list),
+		OPT_SUBCOMMAND("log", &fn, cmd_series_log),
 		OPT_END()
 	};
 
