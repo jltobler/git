681b0d371a2ceb2668cb37241304accc8495f860
Patrick Steinhardt <ps@pks.im>
2026-05-06 14:50:28 +0200

    odb/transaction: add ODB transaction flags
    Certain Git operations require specific configuration of an ODB
    transaction. Introduce an `ODB_TRANSACTION_RECEIVE` flag in preporation
    for using ODB transaction in git-receive-pack(1).
    
    Signed-off-by: Justin Tobler <jltobler@gmail.com>

--- odb/source.h
+++ odb/source.h
@@ -3,6 +3,7 @@
 
 #include "object.h"
 #include "odb.h"
+#include "odb/transaction.h"
 
 enum odb_source_type {
 	/*
@@ -231,7 +232,8 @@ struct odb_source {
 	 * negative error code otherwise.
 	 */
 	int (*begin_transaction)(struct odb_source *source,
-				 struct odb_transaction **out);
+				 struct odb_transaction **out,
+				 enum odb_transaction_flags flags);
 
 	/*
 	 * This callback is expected to read the list of alternate object
@@ -470,9 +472,10 @@ static inline int odb_source_write_alternate(struct odb_source *source,
  * Returns 0 on success, a negative error code otherwise.
  */
 static inline int odb_source_begin_transaction(struct odb_source *source,
-					       struct odb_transaction **out)
+					       struct odb_transaction **out,
+					       enum odb_transaction_flags flags)
 {
-	return source->begin_transaction(source, out);
+	return source->begin_transaction(source, out, flags);
 }
 
 #endif
